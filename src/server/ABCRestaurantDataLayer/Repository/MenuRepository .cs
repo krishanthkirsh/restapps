2ae0484047fb2e4f506a856ee14d448cec3d41e9
tharopan <tharopan@gmail.com>
2019-06-13 15:50:51 +0530

    Update MenuRepository .cs
    Update

--- src/server/ABCRestaurantDataLayer/Repository/MenuRepository .cs
+++ src/server/ABCRestaurantDataLayer/Repository/MenuRepository .cs
@@ -7,74 +7,10 @@ using System.Linq;
 
 namespace ABCRestaurant_Data.Repository
 {
-    public class MenuRepository : IMenuRepository 
+    public class MenuRepository : Repository<Menu>, IMenuRepository 
     {
-        private RestaurantDBContext _dBConn;
-
-        public MenuRepository(RestaurantDBContext dBConn)
-        {
-            this._dBConn = dBConn;
-        }
-
-        public void Add(Menu entity)
-        {
-            _dBConn.Menus.Add(entity);
-        }
-
-        public void Delete(Menu entity)
-        {
-            _dBConn.Menus.Remove(entity);
-        }
-
-        public Menu FindById(int Id)
-        {
-            return _dBConn.Menus.Where(x => x.MenuID == Id).FirstOrDefault();
-        }
-
-        public IEnumerable<Menu> List()
-        {
-            return _dBConn.Menus;
-        }
-
-        public void Update(Menu entity)
-        {
-            _dBConn.Entry(entity).State = EntityState.Modified;
-            _dBConn.SaveChanges();
-        }
-
-        //public void Add(Menu menu)
-        //{
-        //    _dBConn.Menus.Add(menu);
-        //}
-
-        //public bool Delete(int Id)
-        //{
-        //    var removed = false;
-        //    Menu menu = FindById(Id);
-
-        //    if (menu != null)
-        //    {
-        //        _dBConn.Menus.Remove(menu);
-        //        removed = true;
-        //    }
-
-        //    return removed;
-        //}
-
-        //public IEnumerable<Menu> List()
-        //{
-        //    return _dBConn.Menus;
-        //}
-
-        //public Menu FindById(int Id)
-        //{
-        //    return _dBConn.Menus.Where(x => x.MenuID == Id).FirstOrDefault();
-        //}
-
-        //public void Update(Menu menu)
-        //{
-        //    _dBConn.Entry(menu).State = EntityState.Modified;
-        //    _dBConn.SaveChanges();
-        //}
+        public MenuRepository(RestaurantDBContext dBConn) 
+            : baae(dBConn)
+        { }        
     }
 }
