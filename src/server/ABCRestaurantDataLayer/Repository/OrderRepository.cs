b25f4781bad24a9fb502641ee20b3dd2c6feb878
tharopan <tharopan@gmail.com>
2019-06-13 15:53:45 +0530

    Update OrderRepository.cs
    order repo

--- src/server/ABCRestaurantDataLayer/Repository/OrderRepository.cs
+++ src/server/ABCRestaurantDataLayer/Repository/OrderRepository.cs
@@ -8,64 +8,8 @@ namespace ABCRestaurant_Data.Repository
 {
     public class OrderRepository : IOrderRepository
     {
-        private RestaurantDBContext _dBConn;
-
         public OrderRepository(RestaurantDBContext dBConn)
-        {
-            this._dBConn = dBConn;
-        }
-
-        public void Add(Order entity)
-        {
-            _dBConn.Orders.Add(entity);
-        }
-
-        public void Delete(Order entity)
-        {
-            _dBConn.Orders.Remove(entity);
-        }
-
-        public Order FindById(int Id)
-        {
-            return _dBConn.Orders.Where(x => x.OrderID == Id).FirstOrDefault();
-        }
-
-        public IEnumerable<Order> List()
-        {
-            return _dBConn.Orders;
-        }
-
-        public void Update(Order entity)
-        {
-            _dBConn.Entry(entity).State = EntityState.Modified;
-            _dBConn.SaveChanges();
-        }
-
-        //public void AddOrder(Order order)
-        //{
-        //    _dBConn.Orders.Add(order);
-        //}
-
-        //public bool DeleteOrder(int Id)
-        //{
-        //    var removed = false;
-        //    Order order = GetOrderDetails(Id);
-        //    if (order != null)
-        //    {
-        //        _dBConn.Orders.Remove(order);
-        //        removed = true;
-        //    }
-        //    return removed;
-        //}
-
-        //public IEnumerable<Order> GetAllOrders()
-        //{
-        //    return _dBConn.Orders;
-        //}
-
-        //public Order GetOrderDetails(int Id)
-        //{
-        //    return _dBConn.Orders.Where(x => x.OrderID == Id).FirstOrDefault();
-        //}
+            : base (dBConn)
+        { }       
     }
 }
